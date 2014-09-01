0286d9432620dac45b234c0b680b14d63a37587d
tonatoz <tonatoz@gmail.com>
2014-09-01 15:32:05 +0400

    Zipper for wiki - walk?

--- src/wiki_path/core.clj
+++ src/wiki_path/core.clj
@@ -4,32 +4,50 @@
             [net.cgrand.enlive-html :as html]))
 
 (def url "/wiki/SAP")
+(def base-wiki-url "http://ru.m.wikipedia.org")
 
 (defn get-title [url]
-  (let [html (html/html-snippet (:body @(http/get url)))]
+  "Return title of page by url. Need http request."
+  (let [html (html/html-snippet (:body @(http/get (str base-wiki-url url))))]
     (first (:content (first (html/select html [:h1#section_0]))))))
 
 (defn link-pred [link]
+  "Rules for link to wiki document"
   (and (< 6 (count link))
        (= "/wiki/" (subs link 0 6))
        (not (some #(contains? #{\. \:} %) link))))
 
 (defn get-links [url]
+  "Return all wiki links from url page"
   (let [html-page (html/html-snippet (:body @(http/get url)))]
     (filter link-pred (map (comp :href :attrs) (html/select html-page [:div.content :a])))))
 
+(defn construct-path [node]
+  "Return lazy seq of path of page titles from root to current node"
+  (interpose " -> "
+             (map (comp #(str "[" % "]") get-title)
+                  (concat (zip/path node) [(zip/node node)]))))
 (def wiki-zipper
   (zip/zipper
     (constantly true)
-    (fn [node] (seq (get-links (str "http://ru.m.wikipedia.org" node))))
+    (fn [node] (seq (get-links (str base-wiki-url node))))
     nil
     url))
 
 (get-title
-  (str "http://ru.m.wikipedia.org"
-       (-> wiki-zipper
-           (zip/down)
-           (zip/right)
-           (zip/right)
-           (zip/down)
-           (zip/node))))
+  (-> wiki-zipper
+      (zip/down)
+      (zip/right)
+      (zip/right)
+      (zip/down)
+      (zip/node)))
+
+(print
+  (construct-path (-> wiki-zipper
+                      (zip/down)
+                      (zip/right)
+                      (zip/right)
+                      (zip/down))))
+
+; Deep-fisrt is bad algorithm
+(take-while #(not= "/wiki/Microsoft" (zip/node %)) (iterate zip/next wiki-zipper))
