2d3c7bf6ba89677d095f75a3a84c040d6cdcfc2a
tonatoz <tonatoz@gmail.com>
2014-09-01 12:57:15 +0400

    Zipper for wiki

--- src/wiki_path/core.clj
+++ src/wiki_path/core.clj
@@ -1,23 +1,35 @@
 (ns wiki-path.core
-  (:require [org.httpkit.client :as http]
+  (:require [clojure.zip :as zip]
+            [org.httpkit.client :as http]
             [net.cgrand.enlive-html :as html]))
 
-(def url "http://ru.m.wikipedia.org/wiki/SAP")
+(def url "/wiki/SAP")
 
-(def html-page
-  (html/html-snippet
-    (:body @(http/get url))))
-
-(def raw-links
-  (map (comp :href :attrs) (html/select html-page [:div.content :a])))
-
-(def title
-  (first (:content (first (html/select html-page [:h1#section_0])))))
+(defn get-title [url]
+  (let [html (html/html-snippet (:body @(http/get url)))]
+    (first (:content (first (html/select html [:h1#section_0]))))))
 
 (defn link-pred [link]
-  (and (> 6 (count link))
+  (and (< 6 (count link))
        (= "/wiki/" (subs link 0 6))
-       (not (some #(= \. %) link))))
+       (not (some #(contains? #{\. \:} %) link))))
 
-(def links
-  (filter link-pred raw-links))
+(defn get-links [url]
+  (let [html-page (html/html-snippet (:body @(http/get url)))]
+    (filter link-pred (map (comp :href :attrs) (html/select html-page [:div.content :a])))))
+
+(def wiki-zipper
+  (zip/zipper
+    (constantly true)
+    (fn [node] (seq (get-links (str "http://ru.m.wikipedia.org" node))))
+    nil
+    url))
+
+(get-title
+  (str "http://ru.m.wikipedia.org"
+       (-> wiki-zipper
+           (zip/down)
+           (zip/right)
+           (zip/right)
+           (zip/down)
+           (zip/node))))
